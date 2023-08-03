a397222152f69c1f79169d60dbda268f85b0359f
Антон Сахаров <av.sakharov.888@gmail.com>
2023-08-03 20:45:56 +0300

    added mail

--- backend/physact/settings.py
+++ backend/physact/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', default='5432'),
     }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
     # }
 }
 
