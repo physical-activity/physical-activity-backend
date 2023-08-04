b63809c5517dfa557ffd815ae7651f638e32596f
Антон Сахаров <av.sakharov.888@gmail.com>
2023-08-04 11:21:34 +0300

    nginx.conf fixed

--- backend/physact/settings.py
+++ backend/physact/settings.py
@@ -75,20 +75,20 @@ WSGI_APPLICATION = 'physact.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv(
-            'DB_ENGINE', default='django.db.backends.postgresql'
-        ),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('DB_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
-    }
     # 'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': os.getenv(
+    #         'DB_ENGINE', default='django.db.backends.postgresql'
+    #     ),
+    #     'NAME': os.getenv('DB_NAME', default='postgres'),
+    #     'USER': os.getenv('DB_USER', default='postgres'),
+    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+    #     'HOST': os.getenv('DB_HOST', default='db'),
+    #     'PORT': os.getenv('DB_PORT', default='5432'),
     # }
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_USER_MODEL = 'users.CustomUser'
