f077807c0e13ef8794d4818b9fa526b9c471102f
Антон Сахаров <av.sakharov.888@gmail.com>
2023-08-09 20:06:30 +0300

    allauth model fix

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
         UserViewSet.as_view({'post': 'activation'}), name='activate'
     ),
     path('auth/', include('djoser.urls.authtoken')),
+    path('accounts/', include('allauth.urls')),
     path(
         'auth/reset_password/',
         UserViewSet.as_view({'post': 'reset_password'}),
