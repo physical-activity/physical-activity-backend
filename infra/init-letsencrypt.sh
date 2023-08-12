e605cda31b8b77c9677a9bc74a1c8086b49af2f5
Антон Сахаров <av.sakharov.888@gmail.com>
2023-08-12 17:18:53 +0300

    corrected typos

--- infra/init-letsencrypt.sh
+++ infra/init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(easyit.acceleratorpracticum.ru)
+domains=(easyfit.acceleratorpracticum.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="easyit.organization@gmail.com" # Adding a valid address is strongly recommended
+email="easyfit.organization@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
