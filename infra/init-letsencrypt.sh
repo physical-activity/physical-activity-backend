5e535b0f76c6b44fe18d851741675049ea4cd8c7
Антон Сахаров <av.sakharov.888@gmail.com>
2023-08-16 18:33:24 +0300

    new_domain

--- infra/init-letsencrypt.sh
+++ infra/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(easyfit.acceleratorpracticum.ru)
+domains=(easyfit.space)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="easyfit.organization@gmail.com" # Adding a valid address is strongly recommended
